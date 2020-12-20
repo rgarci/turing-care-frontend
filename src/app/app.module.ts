2cfb54172a51691fad73abda8f40d5220f2d684d
Eliezer Couoh <eliezer.couoh@adira.com.mx>
2020-12-19 21:25:40 -0600

    registers-tables and patient details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table'
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule} from '@angular/material/button';
+import { RegistersTableComponent } from './components/registers-table/registers-table.component';
+import { PatientDetailsComponent } from './components/patient-details/patient-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RegistersTableComponent,
+    PatientDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
